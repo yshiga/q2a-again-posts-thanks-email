931fb558e8c505cdd8e6efd17d378d462ec808ec
EC2 Default User <ec2-user@ip-172-31-29-147.ap-northeast-1.compute.internal>
2016-05-23 16:50:14 +0900

    remove unuse logs

--- q2a-again-posts-thanks-email-event.php
+++ q2a-again-posts-thanks-email-event.php
@@ -1,18 +1,8 @@
 <?php
-if (!defined('QA_VERSION')) { 
+if (!defined('QA_VERSION')) {
 	require_once dirname(empty($_SERVER['SCRIPT_FILENAME']) ? __FILE__ : $_SERVER['SCRIPT_FILENAME']).'/../../qa-include/qa-base.php';
    require_once QA_INCLUDE_DIR.'app/emails.php';
 }
-
-// for local-test START
-/*****************************
-qa_opt('q2a-again-posts-thanks-day', 7);
-qa_opt('q2a-again-posts-thanks-body', 'メールの本文です。');
-$obj = new q2a_again_posts_thanks_email_event();
-$param['userid'] = 1589;
-$obj->process_event('q_post', 1589, 'developer', 0, $param);
-*****************************/
-// for local-test END
 
 class q2a_again_posts_thanks_email_event
 {
@@ -23,14 +13,6 @@ class q2a_again_posts_thanks_email_event
 
 		$LIMIT = (int)qa_opt('q2a-again-posts-thanks-day');	// 閾値：日数
 		if ((!is_numeric($LIMIT)) or $LIMIT == '0') {
-// for debug START
-/*******************
-$fp = fopen("/tmp/plugin03.log", "a+");
-$outs = "LIMIT[". $LIMIT. "] ---> not numeric\n";
-fputs($fp, $outs);
-fclose($fp);
-*******************/
-// for debug END
 			return;
 		}
 
@@ -39,16 +21,6 @@ fclose($fp);
 		foreach($posts as $post){
 			$postcount = $post["postcount"];
 		}
-// for debug START
-/*******************
-$fp = fopen("/tmp/plugin03.log", "a+");
-$outs = "--------------------------\n";
-$outs .= "userid[" . $userid. "]\n";
-$outs .= "postcount:".$postcount."\n";
-fputs($fp, $outs);
-fclose($fp);
-*******************/
-// for debug END
 
 		if ($postcount > 0) {
 			$user = $this->getUserInfo($userid);
@@ -56,10 +28,11 @@ fclose($fp);
 			$email = $user[0]['email'];
 			$title = "久しぶりの投稿ありがとうございます。";
 			$bodyTemplate = qa_opt('q2a-again-posts-thanks-body');
-			$body = strtr($bodyTemplate, 
+			$body = strtr($bodyTemplate,
 				array(
 					'^username' => $handle,
-					'^sitename' => qa_opt('site_title')
+					'^sitename' => qa_opt('site_title'),
+					'^siteurl' => qa_opt('site_url')
 				)
 			);
 			$this->sendEmail($title, $body, $handle, $email);
@@ -77,29 +50,9 @@ fclose($fp);
 		$mail_params['toname'] = $toname;
 		$mail_params['toemail'] = $toemail;
 		$mail_params['html'] = false;
-// for debug START
-/*******************
-$fp = fopen("/tmp/plugin03.log", "a+");
-$outs = "fromemail:".$mail_params['fromemail']."\n";
-fputs($fp, $outs);
-$outs = "fromname:".$mail_params['fromname'] . "\n";
-fputs($fp, $outs);
-$outs = "subject:".$mail_params['subject'] . "\n";
-fputs($fp, $outs);
-$outs = "body:".$mail_params['body'] . "\n";
-fputs($fp, $outs);
-$outs = "toname:".$mail_params['toname'] . "\n";
-fputs($fp, $outs);
-$outs = "toemail:".$mail_params['toemail'] . "\n";
-fputs($fp, $outs);
-fclose($fp);
-*******************/
-// for debug END
-
-		qa_send_email($mail_params);
+		//qa_send_email($mail_params);
 
 		$mail_params['toemail'] = 'yuichi.shiga@gmail.com';
-		//$mail_params['toemail'] = 'ryuta_takeyama@nexyzbb.ne.jp';
 		qa_send_email($mail_params);
 	}
 
@@ -119,7 +72,7 @@ fputs($fp, $outs);
 fclose($fp);
 *******************/
 // for debug END
-		$result = qa_db_query_sub($sql); 
+		$result = qa_db_query_sub($sql);
 		return qa_db_read_all_assoc($result);
 	}
 
